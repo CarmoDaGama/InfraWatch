4a781cfbf06b7836f100a4f8ef2b19d567bfe5ff
carmodagama <carmodagama@gmail.com>
2026-03-29 14:59:42 +0100

    Changed url api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         // target: 'http://localhost:3001',
-        target: 'https://infra-watch-drab.vercel.app',
+        target: 'https://infrawatch-a11m.onrender.com',
         changeOrigin: true,
       }
     }
